9c34c64e0442050a44afb765726a73eec0736043
Vinit Surve <115409878+VinitSurve@users.noreply.github.com>
2023-08-10 13:01:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+function cube()
+
+{
+    var a = parseInt(f1.t1.value);
+    // x 1
+    b = a*1;
+    f1.t2.value=+b;
+    c=1*1;
+    f1.t3.value=+c;
+    d=a*1;
+    f1.t4.value=+d;
+    // x 2
+    e=a*1;
+    f1.t5.value=+e;
+    f=1*2;
+    f1.t6.value=+f;
+    g=a*2;
+    f1.t7.value=+g;
+    // x 3
+    h=a*1;
+    f1.t8.value=+h;
+    i=1*3;
+    f1.t9.value=+i;
+    k=a*3;
+    f1.t10.value=+k;
+    // x 4
+    l=a*1;
+    f1.t11.value=+l;
+    m=1*4;
+    f1.t12.value=+m;
+    n=a*4;
+    f1.t13.value=+n;
+    // x 5
+    o=a*1;
+    f1.t14.value=+o;
+    p=1*5;
+    f1.t15.value=+p;
+    q=a*5;
+    f1.t16.value=+q;
+    // x 6
+    r=a*1;
+    f1.t17.value=+r;
+    s=1*6;
+    f1.t18.value=+s;
+    t=a*6;
+    f1.t19.value=+t;
+    // x 7
+    u=a*1;
+    f1.t20.value=+u;
+    v=1*7;
+    f1.t21.value=+v;
+    w=a*7;
+    f1.t22.value=+w;
+    // x 8
+    x=a*1;
+    f1.t23.value=+x;
+    y=1*8;
+    f1.t24.value=+y;
+    z=a*8;
+    f1.t25.value=+z;
+    // x 9
+    a1=a*1;
+    f1.t26.value=+a1;
+    a2=1*9;
+    f1.t27.value=+a2;
+    a3=a*9;
+    f1.t28.value=+a3;
+    // x 10
+    a4=a*1;
+    f1.t29.value=+a4;
+    a5=1*10;
+    f1.t30.value=+a5;
+    a6=a*10;
+    f1.t31.value=+a6;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
